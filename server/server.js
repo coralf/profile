cd888eeb92d1e9c0fb6922a2694ecc90a5ad24b7
thinkingsim <861958334@qq.com>
2020-04-18 00:09:56 +0800

    '首次提交'

---
+++ server/server.js
@@ -0,0 +1,28 @@
+const Koa = require("koa");
+const Router = require("koa-router");
+const bodyParser = require('koa-bodyparser');
+const app = new Koa();
+const router = new Router();
+app.use(bodyParser());
+
+
+router.post("/api/login", async ctx => {
+  const { username, password } = ctx.request.body;
+  console.log(username, password);
+  if (username == "123" && password == "123") {
+    ctx.status = 200;
+    ctx.body = { code: 1, message: "登录成功" };
+  } else {
+    ctx.status = 401;
+    ctx.body = { code: 0, message: "用户名或者密码错误" };
+  }
+});
+
+router.get(
+  "/api/userinfo",
+  async ctx => {
+    ctx.body = { code: 1, data: { name: "jerry", age: 20 } };
+  }
+);
+app.use(router.routes());
+app.listen(3000);
